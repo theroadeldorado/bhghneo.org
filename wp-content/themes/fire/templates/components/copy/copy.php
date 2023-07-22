b015e3810cde31423124a0469135c149ddd5f95a
John Young <theroad2eldorado@gmail.com>
2023-07-21 21:40:15 -0400

    🚀 adds wide option

--- wp-content/themes/fire/templates/components/copy/copy.php
+++ wp-content/themes/fire/templates/components/copy/copy.php
@@ -1,5 +1,6 @@
 <?php
   $copy = get_sub_field('copy');
+  $full_width = get_sub_field('full_width');
   $section->add_classes([
     'py-10 md:py-20'
   ]);
@@ -8,7 +9,7 @@
 <?php $section->start(); ?>
 
   <?php if($copy) : ?>
-    <div class="wizzy container max-w-2xl lg:max-w-4xl">
+    <div class="wizzy container <?php echo $full_width ? '' : 'max-w-2xl lg:max-w-4xl';?>">
       <?php echo $copy; ?>
     </div>
   <?php endif; ?>
