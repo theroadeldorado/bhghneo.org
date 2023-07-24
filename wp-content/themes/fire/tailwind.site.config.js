09f6135ef6dea6e578c327639c460c04a7fdb19b
John Young <theroad2eldorado@gmail.com>
2023-07-23 20:52:18 -0400

    ✅ updates

--- wp-content/themes/fire/tailwind.site.config.js
+++ wp-content/themes/fire/tailwind.site.config.js
@@ -43,7 +43,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        yellow: '#fce100',
+        yellow: '#ffb30d',
         orange: {
           1: '#F68B11',
           2: '#F36917',
