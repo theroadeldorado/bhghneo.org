01810975e7edac72ca24d94d85932e564aa91fef
John Young <theroad2eldorado@gmail.com>
2023-07-31 22:17:57 -0400

    ✅ updates plugin

--- wp-content/themes/fire/templates/components/full-width-image/full-width-image.php
+++ wp-content/themes/fire/templates/components/full-width-image/full-width-image.php
@@ -1,5 +1,7 @@
 <?php
   $image = get_sub_field('image');
+  $scroll = get_sub_field('scroll');
+  $classes = $scroll ? 'mx-auto w-auto min-w-full max-w-[120vw] min-h-[200px] md:min-h-[300px] lg:min-h-[800px]' : 'w-full h-auto rounded-md overflow-hidden';
   $section->add_classes([
     ''
   ]);
@@ -7,8 +9,10 @@
 
 <?php $section->start(); ?>
   <?php if($image):?>
-    <div class="container py-5 md:py-10">
-      <?php new Fire_Picture($image['url'], $image['alt'], [[2000,null],[900,null],[800,null]], 'w-full h-auto rounded-md');?>
+    <div class="py-5 md:py-10">
+      <div class="<?php echo $scroll ? 'flex items-start pb-4 overflow-scroll' : 'container';?>">
+        <?php new Fire_Picture($image['url'], $image['alt'], [[2000,null],[1200,null],[1000,null]], $classes );?>
+      </div>
     </div>
   <?php endif;?>
 <?php $section->end(); ?>
--- wp-content/themes/fire/templates/components/team-group/team-group.php
+++ wp-content/themes/fire/templates/components/team-group/team-group.php
@@ -5,7 +5,7 @@
   $style .= ' mb-10 text-blue-2 text-center';
 
   $section->add_classes([
-    'py-10 md:py-20'
+    'py-5'
   ]);
 ?>
 
@@ -21,7 +21,8 @@
         <?php while ( have_rows('team_members') ) : the_row();
           $name = get_sub_field('name');
           $title = get_sub_field('title');
-          $email = get_sub_field('email');?>
+          $email = get_sub_field('email');
+          $phone_number_extension = get_sub_field('phone_number_extension');?>
           <div class="w-[calc(50%-12px)] md:w-[265px] shrink-0">
             <div class="flex items-center flex-col gap-0.5 md:gap-1 text-center rounded-md bg-gray-50 shadow-md py-3 md:py-6 px-2">
               <?php if($name):?>
@@ -29,6 +30,9 @@
               <?php endif;?>
               <?php if($title):?>
                 <p class="text-[12px] md:text-sm"><?php echo $title;?></p>
+              <?php endif;?>
+              <?php if($phone_number_extension):?>
+                <span class="flex flex-col text-[11px] md:text-sm items-center justify-center">ext: <?php echo $phone_number_extension;?></span>
               <?php endif;?>
               <?php if($email):?>
                 <a href="mailto:<?php echo $email;?>" class="flex flex-col text-[11px] md:text-sm items-center justify-center text-orange-2 hover:underline"><?php echo $email;?></a>
